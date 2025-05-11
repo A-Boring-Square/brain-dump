44b1d03603dd6770a29bbe5ddb6cf365b343a250
A-Boring-Square <123091707+A-Boring-Square@users.noreply.github.com>
2025-05-11 11:24:40 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://a-boring-square.github.io/',
+  base: 'brain-dump',
+});
